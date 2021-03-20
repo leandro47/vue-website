2c92e0d4b2558eaf7a46964d36885e881fe97bb6
leandro47 <leandrosilva47@live.com>
2021-03-20 16:24:38 -0300

    guards globais

--- src/main.js
+++ src/main.js
@@ -9,6 +9,16 @@ import App from './App.vue'
 Vue.use(Vue2Filters)
 Vue.config.productionTip = false
 
+// router.beforeEach((to, from, next) =>{
+//   console.log(to, from);
+
+//   next();
+// });
+
+// router.afterEach(() => {
+//   console.log('Renderização ok');
+// })
+
 new Vue({
   render: h => h(App),
   store,
